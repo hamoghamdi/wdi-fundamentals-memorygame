23ba38a1a22433d5975cd60d37da55e4d09524f9
mac <mac@macs-MacBook-Pro.local>
2019-04-19 14:33:39 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,19 @@
 console.log("Up and running!");
 
-
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardTwo);
-
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  { rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png" },
+  { rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png" },
+  { rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png" },
+  { rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png" }
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -17,8 +26,11 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-  console.log("User flipped " + cards[cardId])
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId.rank])
+  cardsInPlay.push(cards[cardId].rank);
+
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
 }
 
