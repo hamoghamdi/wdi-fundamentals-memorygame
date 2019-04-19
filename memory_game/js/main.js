bfb34206fda24239af388f5d131fcf1d356d23ad
mac <mac@macs-MacBook-Pro.local>
2019-04-19 12:43:55 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,18 +7,20 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped king");
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2){
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+function checkForMatch(){
+  if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+      console.log("You found a match!");
+    }
+    else { console.log("Sorry, try again.") }
   }
-  else { alert("Sorry, try again.") }
 }
+
+function flipCard(cardId){
+  console.log("User flipped " + cards[cardId])
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
