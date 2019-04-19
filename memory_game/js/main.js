5b01affe515a74f9a2f45443e6357f2736e36954
mac <mac@macs-MacBook-Pro.local>
2019-04-19 18:35:30 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -16,23 +16,37 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-function checkForMatch(){
+
+function checkForMatch(){ //alert
   if (cardsInPlay.length === 2){
     if (cardsInPlay[0] === cardsInPlay[1]){
-      console.log("You found a match!");
+      alert("You found a match!");
     }
-    else { console.log("Sorry, try again.") }
+    else { alert("Sorry, try again."); }
   }
 }
 
-function flipCard(cardId){
-  console.log("User flipped " + cards[cardId.rank])
+ function flipCard(){
+  //'this' is the cardElement object (the clicked card)
+  var cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
-
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+  // replace the back image with the card image
+  this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+     cardElement.setAttribute('data-id', i);
+     cardElement.addEventListener('click', flipCard);
+     document.getElementById('game-board').appendChild(cardElement);
+}
+}
+
+createBoard();
+
+// Bonus
+// add a "Reset" button to the HTML and set up an event in the JavaScript file so that the user can reset the game after playing.
+// For an extra challenge, consider how you could keep track of and display the user's score.
